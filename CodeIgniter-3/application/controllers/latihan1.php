cd058d67a20075d6205be62522ed1c353000b69f
juanpolopurba <juanpolopurba05.9@gmail.com>
2022-04-06 00:51:16 +0700

    tugaswp2

---
+++ CodeIgniter-3/application/controllers/latihan1.php
@@ -0,0 +1,11 @@
+<?php
+
+class latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat datang,selamat belajar web programing";
+    }
+    
+}
+?>
---
+++ CodeIgniter-3/application/models/model_latihan1.php
@@ -0,0 +1,10 @@
+<?php
+class model_latihan1 extends CI_Model
+{
+    public $nilai1, $nilai2, $hasil;
+
+    public funtion jumlah($nil1=null, $nil2=null)
+    {
+        $this 
+    }
+}
